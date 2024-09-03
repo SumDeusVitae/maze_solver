202ecd1cbcab43ac3b25435c138125cd87806cda
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-09-03 10:42:54 -0500

    Add class Cell, Line, Point with their methods

--- graphics.py
+++ graphics.py
@@ -9,6 +9,9 @@ class Window:
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -22,3 +25,39 @@ class Window:
 
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x: int=0, y: int=0):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self,point1 , point2) -> None:
+        self.point1 = point1
+        self.point2 = point2
+    
+    def draw(self, Canvas, fill_color):
+        Canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill = fill_color, width = 2)
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+
+        self._win = window
+    def draw(self):
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), "black")
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), "black")
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), "black")
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), "black")
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from graphics import Window
+from graphics import Window, Line, Point, Cell
+
+
 
 def main():
     win = Window(800,600)
+    cell1 = Cell(50, 50, 75, 75, win)
+    cell2 = Cell(100, 100, 150, 150, win)
+
+    
+    # Draw the cell
+    cell1.draw()
+    cell2.has_left_wall = False
+    cell2.draw()
+
+    
     win.wait_for_close()
 
-main()
+if __name__ == "__main__":
+    main()
